416e96a9f89e2801716ca7067957ed0732803d53
el-indio <el-indio@el-indio.sad-hill>
2009-05-14 20:50:27 +0200

    besoin d'un café

--- src/idc/IDCManager.java
+++ src/idc/IDCManager.java
@@ -30,7 +30,7 @@ public class IDCManager {
 
 	static public List friends; // Connexions directes
 
-	static private List channels;
+	static public List channels;
 
 	static private Hashtable<Node, Queue<InetAddress>> gate;
 
@@ -160,7 +160,7 @@ public class IDCManager {
 
 		String me = new String(myNode.getId());
 		String targ = new String(req.getTarget());
-
+		System.out.println("TARGET EQUALS ?"+me+" --------- "+targ+" ------ "+new String (req.getSource()));
 		if (!me.contentEquals(new StringBuffer(targ))) {
 			System.out.println("Request not for me !");
 			return;
@@ -204,7 +204,7 @@ public class IDCManager {
 			
 			//System.out.println("name of the requester :"+nodes.get("{"+new String( req.getSource())).getNickname());
 			answer.setAsAnswer(true);
-			acceptation.setNom(nodes.get( new String(myNode.getId()) ).getNickname());
+			acceptation.setNom(nodes.get( new String(req.getSource()) ).getNickname());
 			acceptation.setVisible(true);
 			while(accept.click == -1){};
 			send(answer);
